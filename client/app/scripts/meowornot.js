63c6d980185ecddb07f5a432069d2a40dc9c4cbb
System Administrator <root@Walters-MacBook-Pro.local>
2014-03-29 22:48:54 -0400

    cat update

--- client/app/scripts/meowornot.js
+++ client/app/scripts/meowornot.js
@@ -1,1 +1,18 @@
-alert("hello world");
+var Cat = {};
+Cat.get = function(name)
+{
+	return "temp";
+};
+
+var cat = new Cat();
+
+var desc = cat.get("description");
+var isAdoptable = cat.get("is_adoptable");
+var location = cat.get("location");
+var breed = cat.get("breed");
+var size = cat.get("size");
+var sex = cat.get("sex");
+var age = cat.get("age");
+var imageUrl = cat.get("image_url");
+
+$('#cat').innerHTML = desc ;
