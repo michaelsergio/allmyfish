c4981f3c6e55930834d9baeee69c07b6a955e0f1
Michael Sergio <mikeserg@gmail.com>
2014-03-30 02:04:58 -0400

    Follows specs more

--- client/app/scripts/meowornot.js
+++ client/app/scripts/meowornot.js
@@ -1,36 +1,61 @@
 $(document).ready(function() {
   var Cat = Backbone.Model.extend({
-    url: function() {
-      return "http://plentyofcats.com/poc/cats.php?id=1";
-    }
+
+    sync: function(method, model, options) {
+      if (method.toLowerCase() === 'read') {
+        options = options || {};
+        options.url = "http://plentyofcats.com/poc/testDB.php?random=YES";
+      }
+      return Backbone.sync.apply(this, arguments);
+    },
+
+    meow: function(onSuccess) {
+      this.increment("meow_count", onSuccess);
+    },
+    notmeow: function(onSuccess) {
+      this.increment("not_count", onSuccess);
+    },
+    increment: function(field, onSuccess) {
+      var url = "http://plentyofcats.com/poc/cats.php?id=1" + "&field" + field;
+      _.bind($.get(url, {'id':this.id}, onSuccess), this);  
+    },
+
   });
-  Cat.sync();
+  var cat = new Cat();
+  cat.fetch();
 
-  /*
-  var Cat = {};
-  Cat.get = function(name)
-  {
-    return "temp";
-  };
+  var profile = cat.get("description");
+  // Pop up profile on profile click 
+  // Maybe display short bit here
 
-  var cat = Cat;
-*/
-  var desc = cat.get("description");
-  var isAdoptable = cat.get("is_adoptable");
-  var location = cat.get("location");
-  var breed = cat.get("breed");
-  var size = cat.get("size");
-  var sex = cat.get("sex");
-  var age = cat.get("age");
+  //var isAdoptable = cat.get("is_adoptable");
+  // var location = cat.get("location");
+  var description = [ cat.get("age"), "-", cat.get("size"), " - ", 
+      cat.get("sex"), cat.get("breed")].join(" ");
+
+
   var imageUrl = cat.get("image_url");
-
   $('#cat-img').attr('src', imageUrl);
 
-  // meow button on click should make and ajax call increment
+  function nextPage() {
+    // add class to slide cat off 
+    // redirect page
+    // on page load slide cat in
+  }
+
+  // meow button on click should make and ajax call an increment
   // on success it should slide in the next cat picture/profile 
   $('#meow').click(function() {
+      cat.meow(function() {
+        nextPage();
+      });
+  });
+  $('#notmeow').click(function() {
+      cat.notmeow(function() {
+        nextPage();
+      });
   });
   
-  // top scores should be displayed
+  // top scores should be displayed on different page
   // scores are just the sum of the cats likes
 });
